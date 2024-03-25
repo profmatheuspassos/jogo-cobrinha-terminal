f31797d87732d1162c0f1b21466f8a8593299f5b
Matheus Passos <40068795+profmatheuspassos@users.noreply.github.com>
2024-03-25 07:38:17 +0200

    Update jogo-cobrinha.py

--- jogo-cobrinha.py
+++ jogo-cobrinha.py
@@ -1,10 +1,22 @@
-import time
 import curses
 
-def gameLoop():
-    for i in range(10):
-        print(f"O valor de i é {i}.")
-        time.sleep(1)
-
+def gameLoop(window):
+    window.addstr(f"Pressione alguma tecla:")
+    while True:
+        window.timeout(1000)
+        char = window.getch()
+        window.clear()
+        match char:
+            case curses.KEY_UP:
+                window.addstr(f"Mover para cima")
+            case curses.KEY_LEFT:
+                window.addstr(f"Mover para esquerda")
+            case curses.KEY_DOWN:
+                window.addstr(f"Mover para baixo")
+            case curses.KEY_RIGHT:
+                window.addstr(f"Mover para direita")
+            case _:
+                window.addstr(f"Não mover")
+        
 if __name__ == "__main__":
     curses.wrapper(gameLoop)
